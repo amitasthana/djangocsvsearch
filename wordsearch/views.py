2bc747e73bd3c5d6e84e0278f8cfcfe89e2bde6e
Amit Asthana <amitasthana.777@gmail.com>
2018-10-27 05:23:44 +0530

    final solution with sorting and login

--- wordsearch/views.py
+++ wordsearch/views.py
@@ -38,37 +38,47 @@ class WordSearch(APIView):
     """docstring for WordSearch"""
     permission_classes = ((AllowAny,))
     objectify = True
-    #lookup_url_kwarg = 's'
-
     
-
     def get(self,request,*args,**kwargs):
         word = request.GET['word']    
-        #searchword = self.kwargs['s']
         searchword = word
-        
         resultdict = {}
         namelist = []
         datalist = []
         text1 = []
+
+        #reading the file and taking it in the array
         with open(settings.FILES_ROOT + 'word_search.tsv') as csvfile:
             reader = csv.DictReader(csvfile, delimiter='\t')
             for row in reader:
-                namelist.append( row['name'])
-                datalist.append( ( {'name':row['name'] } , {'frequency' : row['frequency']  }) )
-                text1.append( (row['name'] , int(  row['frequency'] ) ) )
+                datalist.append( ( {'name':row['name'] , 'frequency' : int(  row['frequency'] ) }) )
 
+        
+        #creating a sorted list as per the frequency        
+        newlist = sorted(datalist, key=lambda k: k['frequency'], reverse=True) 
+        
+        #just taking the sorted list which sorted by frequency in namelist
+        for f in newlist:
+            namelist.append(f['name'])
+
+
+        #Doing the searching of the word in the list which is sorted by frequency    
         matching = [s for s in namelist if searchword in s]
 
+        #in the mathing now finding the elements which start with the search word
+        result_startwith = list(  filter(lambda x: x.startswith(searchword), matching))
+        sortlist = sorted ( result_startwith)        
+        
+        #creating a array which excludes all the elements which starts with the search word
+        l3 = set(  matching ) - set(  list(result_startwith ) )
+        
 
-        fdist1 = FreqDist(text1)
-
-        #pprint(text1)
-
-        #sortlist = sorted(matching)
-
-
-        return Response(matching,status.HTTP_200_OK)
+        #creating a new list first part is the list which is sorted by the starting of the search word
+        #Taking only 25 elemets form the new list            
+        mergedlist = (  sortlist + list(l3) )[:25]    
+            
+        #sending in response the first 25 results
+        return Response(mergedlist,status.HTTP_200_OK)
         
 
 
